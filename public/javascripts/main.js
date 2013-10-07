767b861efbaae7b35000742e527a715e33af4b85
Alexander Roberts <aj_roberts_50k@msn.com>
2013-10-07 16:51:13 -0400

    Implemented sliding dropdown menues with Codys jQuery patch. Now dropdown is problematic when navbar is collapsed.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,3 +2,12 @@
 *	Main JS Loop
 */
 
+/*
+*   $('.navbar .nav.navbar-nav li.dropdown a') Tells jQuery to select all a tags inside an li.dropdown(that are inside a .nav.navbar-nav)
+*   .on('EVENT', FUNCTION(){ THE FUNCTION WE RUN WHEN EVENT IS FIRED });
+*/
+$('.navbar .nav.navbar-nav li.dropdown a').on('click', function(e) {
+     $(e.target).parent().children('ul.dropdown-menu').slideToggle();
+ 
+});
+ 
