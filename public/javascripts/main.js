25a2f51b13233511146d72d23493c4747af5cc09
Alexander Roberts <aj_roberts_50k@msn.com>
2013-10-18 19:05:31 -0400

    Reorganized and condensed slidedown menu styling

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -7,7 +7,8 @@
 *   .on('EVENT', FUNCTION(){ THE FUNCTION WE RUN WHEN EVENT IS FIRED });
 */
 $('.navbar .nav.navbar-nav li.dropdown a').on('click', function(e) {
-     $(e.target).parent().children('ul.dropdown-menu').slideToggle();
- 
+    $(e.target).parent().children('ul.dropdown-menu').slideToggle(300, function() {
+        $(e.target).parent().toggleClass("open");
+    }); 
 });
  
